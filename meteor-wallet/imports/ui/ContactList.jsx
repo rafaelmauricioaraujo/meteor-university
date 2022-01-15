be3be305358501d770124c28360403c3d8add64f
Rafael Araujo <raraujo@drcontactlens.com>
2022-01-15 12:40:31 -0300

    Removing dependencies not used

--- meteor-wallet/imports/ui/ContactList.jsx
+++ meteor-wallet/imports/ui/ContactList.jsx
@@ -1,15 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useTracker } from "meteor/react-meteor-data";
 import ContactCollection from "../api/ContactCollection";
 
 export default ContactList = () => {
-  // const [contactList, setContactList] = useState([]);
-
-  // useEffect(() => {
-  //   const response = ContactCollection.find({}).fetch();
-  //   setContactList(response);
-  // }, []);
-
   const contactList = useTracker(() => {
     return ContactCollection.find({}).fetch();
   });
