58dfcab09a2d025e8a00e9379737c6e0b9d7f4ce
Rafael Araujo <raraujo@drcontactlens.com>
2022-01-15 11:17:00 -0300

    Starting a new project meteor wallet

---
+++ meteor-wallet/imports/ui/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ContactForm from "./ContactForm";
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor Wallet!</h1>
+    <ContactForm />
+  </div>
+);
---
+++ meteor-wallet/imports/ui/ContactForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+export default ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const saveContact = (event) => {
+    event.preventDefault();
+    console.log(name, email, imageUrl);
+    setName("");
+    setEmail("");
+    setImageUrl("");
+  };
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="email">Name</label>
+        <input
+          id="email"
+          type="text"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="imageUrl">ImageUrl</label>
+        <input
+          id="imageUrl"
+          type="text"
+          value={imageUrl}
+          onChange={(event) => setImageUrl(event.target.value)}
+        ></input>
+      </div>
+      <div>
+        <button onClick={saveContact}>Save Contact</button>
+      </div>
+    </form>
+  );
+};
