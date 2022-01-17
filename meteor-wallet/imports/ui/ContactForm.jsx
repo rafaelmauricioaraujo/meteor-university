29b5889532a4183aa8225414a4350e6f59e82d8d
Rafael Araujo <raraujo@drcontactlens.com>
2022-01-17 10:39:17 -0300

    Adding tailwind css styles

--- meteor-wallet/imports/ui/ContactForm.jsx
+++ meteor-wallet/imports/ui/ContactForm.jsx
@@ -6,8 +6,7 @@ export default ContactForm = () => {
   const [email, setEmail] = useState("");
   const [imageUrl, setImageUrl] = useState("");
 
-  const saveContact = (event) => {
-    event.preventDefault();
+  const saveContact = () => {
     ContactCollection.insert({ name, email, imageUrl });
     setName("");
     setEmail("");
@@ -15,36 +14,64 @@ export default ContactForm = () => {
   };
 
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        ></input>
+    <form className="mt-6">
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label
+            htmlFor="imageUrl"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Image URL
+          </label>
+          <input
+            type="text"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="text"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        ></input>
-      </div>
-      <div>
-        <label htmlFor="imageUrl">ImageUrl</label>
-        <input
-          id="imageUrl"
-          type="text"
-          value={imageUrl}
-          onChange={(event) => setImageUrl(event.target.value)}
-        ></input>
-      </div>
-      <div>
-        <button onClick={saveContact}>Save Contact</button>
+      <div className="px-2 py-3 text-right">
+        <button
+          type="button"
+          onClick={saveContact}
+          className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
+          Save Contact
+        </button>
       </div>
     </form>
   );
--- meteor-wallet/imports/ui/ContactList.jsx
+++ meteor-wallet/imports/ui/ContactList.jsx
@@ -7,15 +7,41 @@ export default ContactList = () => {
     return ContactCollection.find({}).fetch();
   });
   return (
-    <>
-      <h2>Contact List</h2>
-      <ul>
-        {contactList.map((contact) => (
-          <li key={contact.email}>
-            {contact.name} - {contact.email}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <div className="mt-10">
+        <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
+          Contact List
+        </h3>
+        <ul
+          role="list"
+          className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200"
+        >
+          {contactList.map((person, personIdx) => (
+            <li
+              key={personIdx}
+              className="py-4 flex items-center justify-between space-x-3"
+            >
+              <div className="min-w-0 flex-1 flex items-center space-x-3">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={person.imageUrl}
+                    alt=""
+                  />
+                </div>
+                <div className="min-w-0 flex-1">
+                  <p className="text-sm font-medium text-gray-900 truncate">
+                    {person.name}
+                  </p>
+                  <p className="text-sm font-medium text-gray-500 truncate">
+                    {person.email}
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
